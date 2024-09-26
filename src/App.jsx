3742b2e5f520cb874054bea416417692aba3f70c
Prashant29062003 <112088696+Prashant29062003@users.noreply.github.com>
2024-09-26 11:30:34 +0000

    appwrite all prerequisits done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  
+  return (
+    <>
+      <h1 className="w-full bg-purple-600 text-white font-medium text-center text-4xl p-4">Hello</h1>
+    </>
+  )
+}
+
+export default App
